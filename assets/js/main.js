5fe441c9ff1be2490aa9999261f4aefc138739fc
Mohin Rana <ebrahimit49@gmail.com>
2023-07-13 15:51:17 +0600

    Responsive product article

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,9 @@ $(document).ready(() => {
     $(".categoryFinder").click((e) => {
         $(".leftMenuWrap").toggleClass(["left-0"])
         $(".leftMenuWrap").toggleClass("-left-[250px]")
-        $(".main").toggleClass("pl-[250px]")
+        if(  window.innerWidth > 992 ){
+            $(".main").toggleClass("pl-[250px]")
+        }
     })
     $(".closeLeftMenu").click((e) => {
         $(".leftMenuWrap").addClass("-left-[250px]")
@@ -15,3 +17,4 @@ $(document).ready(() => {
         $(this).parents('a').next().slideToggle();
     })
 })
+
