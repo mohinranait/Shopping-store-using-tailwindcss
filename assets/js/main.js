63e2f4483a53d79d07cc55189003c47ae9fe08c0
Mohin Rana <ebrahimit49@gmail.com>
2023-07-15 00:08:28 +0600

    Product gallery jquery animation added

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,12 +4,12 @@ $(document).ready(() => {
         $(".leftMenuWrap").toggleClass("-left-[250px]")
         if(  window.innerWidth > 992 ){
             $(".main").toggleClass("boxLayout")
+            // Product details page jquery code start
             $(".productTab").toggleClass("lg:col-span-9")
-            // $(".productTab").toggleClass("lg:col-span-12")
             $(".productAside").toggleClass("lg:block")
-            // $(".productAside").toggleClass("hidden")
             $(".productGallary").toggleClass("grid-cols-12")
             $(".productOffer").toggleClass("lg:col-span-3")
+            // Product details page jquery code end
         }
     })
     $(".closeLeftMenu").click((e) => {
@@ -17,16 +17,32 @@ $(document).ready(() => {
         $(".leftMenuWrap").removeClass("left-0")
         $(".main").removeClass("boxLayout")
         if( window.innerWidth > 992 ){
+            // Product details page jquery code start
             $(".productTab").toggleClass("lg:col-span-9")
             $(".productAside").removeClass("hidden")
             $(".productGallary").addClass("grid-cols-12")
             $(".productOffer").addClass("lg:col-span-3")
+            // Product details page jquery code end
         }
     })
 
     // Submenu open close
     $(".subMenuFinder").click( function() {
         $(this).parents('a').next().slideToggle();
+    })
+
+    // Product Gallery image jquery code start
+    $(".productGallaryImg").click((e) => {
+        let mainImg = $("#productDisplayImg");
+        mainImg.animate({
+            opacity:0
+        })
+        setTimeout(() => {
+            mainImg.attr('src', e.target.src);
+            mainImg.animate({
+                opacity:1
+            })
+        }, 400);
     })
 })
 
