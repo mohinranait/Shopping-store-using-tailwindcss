ff4abd6d2076d769fe02285573a98ddbf9ffd2e1
Mohin Rana <ebrahimit49@gmail.com>
2023-07-14 22:30:45 +0600

    Product boxed layout responsive design

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,13 +3,25 @@ $(document).ready(() => {
         $(".leftMenuWrap").toggleClass(["left-0"])
         $(".leftMenuWrap").toggleClass("-left-[250px]")
         if(  window.innerWidth > 992 ){
-            $(".main").toggleClass("ml-[250px]")
+            $(".main").toggleClass("boxLayout")
+            $(".productTab").toggleClass("lg:col-span-9")
+            // $(".productTab").toggleClass("lg:col-span-12")
+            $(".productAside").toggleClass("lg:block")
+            // $(".productAside").toggleClass("hidden")
+            $(".productGallary").toggleClass("grid-cols-12")
+            $(".productOffer").toggleClass("lg:col-span-3")
         }
     })
     $(".closeLeftMenu").click((e) => {
         $(".leftMenuWrap").addClass("-left-[250px]")
         $(".leftMenuWrap").removeClass("left-0")
-        $(".main").removeClass("ml-[250px]")
+        $(".main").removeClass("boxLayout")
+        if( window.innerWidth > 992 ){
+            $(".productTab").toggleClass("lg:col-span-9")
+            $(".productAside").removeClass("hidden")
+            $(".productGallary").addClass("grid-cols-12")
+            $(".productOffer").addClass("lg:col-span-3")
+        }
     })
 
     // Submenu open close
