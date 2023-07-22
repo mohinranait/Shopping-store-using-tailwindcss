28ac40007bb95ab9b48396689180bd2f8cf7b029
Mohin Rana <ebrahimit49@gmail.com>
2023-07-23 01:17:02 +0600

    Product Slider added

--- assets/plugins/slick-slider/slider.responsive.js
+++ assets/plugins/slick-slider/slider.responsive.js
@@ -69,3 +69,42 @@ $("#categorySlider").slick({
   
     }]
 });
+
+$("#productSlider").slick({
+
+    infinite: true,
+    autoplay:true,
+    arrows:false,
+    slidesToShow:5,
+    adaptiveHeight:false,
+    responsive: [{
+
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 4,
+          infinite: true
+        }
+  
+      },{
+
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          infinite: true
+        }
+  
+      }, {
+  
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          dots: false
+        }
+  
+      }, {
+  
+        breakpoint: 300,
+        settings: "unslick" // destroys slick
+  
+    }]
+});
